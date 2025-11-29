73481e1ad191e3e70ffc7d20a4de5f8a0fc3e80f
Tez28 <courtezcannady@gmail.com>
2025-11-29 17:58:02 -0500

    Polish CTA section and wire into homepage

--- apps/digitalhooligan-web/app/page.tsx
+++ apps/digitalhooligan-web/app/page.tsx
@@ -1,21 +1,23 @@
-import Hero from "@/components/sections/Hero";
-import Apps from "@/components/sections/Apps";
-import Labs from "@/components/sections/Labs";
-import About from "@/components/sections/About";
-import StreetCred from "@/components/sections/StreetCred";
-import CTA from "@/components/sections/CTA";
-import Contact from "@/components/sections/Contact";
+import Hero from "../components/sections/Hero";
+import AppsSection from "../components/sections/Apps";
+import LabsSection from "../components/sections/Labs";
+import AboutSection from "../components/sections/About";
+import StreetCredSection from "../components/sections/StreetCred";
+import CTASection from "../components/sections/CTA";
+import ContactSection from "../components/sections/Contact";
+import Footer from "../components/layout/Footer";
 
 export default function HomePage() {
   return (
-    <>
+    <main className="bg-dh-black text-dh-offwhite">
       <Hero />
-      <Apps />
-      <Labs />
-      <About />
-      <StreetCred />
-      <CTA />
-      <Contact />
-    </>
+      <AppsSection />
+      <LabsSection />
+      <AboutSection />
+      <StreetCredSection />
+      <CTASection />
+      <ContactSection />
+      <Footer />
+    </main>
   );
 }
